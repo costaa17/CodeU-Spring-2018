0be223514c3901a5a4b038a268301324840e9e78
Costaa17 <toiavalle@gmail.com>
2018-05-17 12:43:57 -0700

    Included tests for the activity feed page and fixed comments

--- src/main/java/codeu/controller/ActivityFeedServlet.java
+++ src/main/java/codeu/controller/ActivityFeedServlet.java
@@ -1,13 +1,10 @@
 package codeu.controller;
 
-import codeu.model.data.Conversation;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 /**
  * Created by CostaA17 on 5/16/18.
@@ -15,12 +12,12 @@ import java.util.List;
 public class ActivityFeedServlet extends HttpServlet {
 
     /**
-     * This function fires when a user navigates to the conversations page. It gets all of the
-     * conversations from the model and forwards to conversations.jsp for rendering the list.
+     * This function fires when a user navigates to the activity feed page.
      */
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
         request.getRequestDispatcher("/WEB-INF/view/activityfeed.jsp").forward(request, response);
     }
+
 }
