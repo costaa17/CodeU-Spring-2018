a0840b9b7f4d4a438662a302c808a7b7e2a3a4fe
Alexandria Storm <alexandriastorm99@gmail.com>
2018-05-22 20:04:10 -0700

    Now committing changes to my local branch.

---
+++ src/main/webapp/WEB-INF/view/ProfilePageServelt.java
@@ -0,0 +1,25 @@
+package codeu.controller;
+
+import codeu.model.data.Conversation;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by alexandriastorm on 5/22/18.
+ */
+public class ProfilePageServlet extends HttpServlet {
+
+    /**
+     * This function fires when a user navigates to a user's profile page. It gets all of the
+     * user's information and bio from the model and forwards to profiles.jsp for rendering the list.
+     */
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        request.getRequestDispatcher("/WEB-INF/view/profiles.jsp").forward(request, response);
+    }
+}
